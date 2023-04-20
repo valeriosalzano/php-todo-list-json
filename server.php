d2c8339a61514b3487ab93bd64b199b8aff5498c
Valerio Salzano <vlr.slzn@gmail.com>
2023-04-20 17:41:42 +0200

    added a data file to store todos, added a gitignore file

--- server.php
+++ server.php
@@ -1,23 +1,28 @@
 <?php
 
-  $todoList = [
-    [
-      'text' => 'todo n.1',
-      'done' => false,
-    ],[
-      'text' => 'todo n.2',
-      'done' => true,
-    ],[
-      'text' => 'todo n.3',
-      'done' => false,
-    ],[
-      'text' => 'todo n.4',
-      'done' => false,
-    ],[
-      'text' => 'todo n.5',
-      'done' => true,
-    ],
-  ];
+  if(file_exists('todoList.json')){
+    $todoListJson = file_get_contents('./todoList.json');
+    $todoList = json_decode($todoListJson, true);
+  } else {
+    $todoList = [
+      [
+        'text' => 'todo n.1',
+        'done' => false,
+      ],[
+        'text' => 'todo n.2',
+        'done' => true,
+      ],[
+        'text' => 'todo n.3',
+        'done' => false,
+      ],[
+        'text' => 'todo n.4',
+        'done' => false,
+      ],[
+        'text' => 'todo n.5',
+        'done' => true,
+      ],
+    ];
+  }
 
   if(isset($_POST) && !empty($_POST['text'])){
     $todoList[] = [ 
@@ -25,7 +30,9 @@
       'done' => $_POST['done'] == 'false' ? false : true,
     ];
   }
+  $todoListJson = json_encode($todoList);
+
+  file_put_contents('todoList.json',$todoListJson);
 
   header('Content-Type: application/json');
-
-  echo json_encode($todoList);
+  echo ($todoListJson);
