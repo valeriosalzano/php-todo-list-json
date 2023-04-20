229105509051a14e4c4d7fe9cf24457a60e5922f
Valerio Salzano <vlr.slzn@gmail.com>
2023-04-20 16:26:20 +0200

    post request done, working on bonus

--- main.js
+++ main.js
@@ -5,7 +5,8 @@ createApp({
   data() {
     return {
       todoList: [],
-      serverUrl: 'server.php'
+      serverUrl: 'server.php',
+      newTodo: '',
     }
   },
   mounted() {
@@ -14,7 +15,26 @@ createApp({
   methods: {
     getTodoList(){
       axios.get(this.serverUrl)
-      .then(response => this.todoList = response.data);
+      .then(response => this.todoList = response.data)
+    },
+    addTodoCheck(){
+      if(this.newTodo && this.newTodo.trim() != ''){
+        this.addTodo();
+      }
+    },
+    addTodo(){
+      const data = {
+        newTodo: this.newTodo,
+      };
+
+      axios.post(this.serverUrl, data,
+        {
+          headers: { 'Content-Type': 'multipart/form-data' }
+        }).then(response => {
+          this.todoList = response.data;
+          console.log(response.data);
+          this.newTodo = '';
+        })
     }
   },
 }).mount('#app')
--- server.php
+++ server.php
@@ -1,24 +1,28 @@
 <?php
 
-  $todolist = [
+  $todoList = [
     [
-      'text' => 'todo n.',
+      'text' => 'todo n.1',
       'done' => false,
     ],[
-      'text' => 'todo n.',
+      'text' => 'todo n.2',
       'done' => true,
     ],[
-      'text' => 'todo n.',
+      'text' => 'todo n.3',
       'done' => false,
     ],[
-      'text' => 'todo n.',
+      'text' => 'todo n.4',
       'done' => false,
     ],[
-      'text' => 'todo n.',
+      'text' => 'todo n.5',
       'done' => true,
     ],
   ];
 
+  if(isset($_POST['newTodo']) && !empty($_POST['newTodo'])){
+    $todoList[] = [ 'text' => $_POST['newTodo'], 'done' => false,];
+  }
+
   header('Content-Type: application/json');
 
-  echo json_encode($todolist);
+  echo json_encode($todoList);
